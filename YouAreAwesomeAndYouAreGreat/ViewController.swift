6379f4a1859ffe9ca3267467291fb2f4c46ae687
Lenny Dance <44321077+lennydance@users.noreply.github.com>
2024-02-21 09:33:31 -0800

    2.3 Challenge Added two buttons and message

--- YouAreAwesomeAndYouAreGreat/ViewController.swift
+++ YouAreAwesomeAndYouAreGreat/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showAwesomeMessage(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+    }
+    @IBAction func showGreatMessage(_ sender: UIButton) {
+        messageLabel.text="You Are Great!"
+    }
+    
 
 }
 
