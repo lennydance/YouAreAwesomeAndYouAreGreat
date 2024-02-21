9bb9067be4507458baf7d23c8c0d55ed5fcdf395
Lenny Dance <44321077+lennydance@users.noreply.github.com>
2024-02-21 09:21:14 -0800

    Initial Commit

---
+++ YouAreAwesomeAndYouAreGreat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouAreAwesomeAndYouAreGreat
+//
+//  Created by Lenny Dance on 2/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
