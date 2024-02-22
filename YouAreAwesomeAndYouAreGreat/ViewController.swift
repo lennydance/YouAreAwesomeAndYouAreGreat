e8045068e67f1a3b82b0f7eecd79b29925e5d286
Lenny Dance <44321077+lennydance@users.noreply.github.com>
2024-02-22 09:03:45 -0800

    Added textAlignment to the label

--- YouAreAwesomeAndYouAreGreat/ViewController.swift
+++ YouAreAwesomeAndYouAreGreat/ViewController.swift
@@ -16,11 +16,18 @@ class ViewController: UIViewController {
     }
     @IBAction func showAwesomeMessage(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = .blue
+        messageLabel.textAlignment = .left
     }
     @IBAction func showGreatMessage(_ sender: UIButton) {
-        messageLabel.text="You Are Great!"
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
     }
     
-
+// Update swift code so that:
+    //When user clicks on show message button, the text is aligned to the left and...
+    // When user clicks on another message, text aligns to the right.
+    // Use the interface builder attributes inspector to change the font size of the messageLabel to a smaller point size, like 24.
 }
 
